9d46360e3f21acf21d8fd85c23586177e7535ced
Nirmalya Sengupta <giterator@gmail.com>
2017-05-11 17:20:54 +0530

    Formatted for better readability

--- app/controllers/AttendeesController.java
+++ app/controllers/AttendeesController.java
@@ -36,12 +36,6 @@ import com.google.inject.Singleton;
 
 @Singleton
 public class AttendeesController extends Controller {
-	/**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
 	
     private final ActorSystem actorSystem;
 	
